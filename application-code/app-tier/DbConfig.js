28cb7da31ad4a9bca4f5466c13c9b847597c3768
Madhusudan V B <madhusudanvb25@gmail.com>
2025-05-16 11:46:38 +0530

    Update DbConfig.js

--- application-code/app-tier/DbConfig.js
+++ application-code/app-tier/DbConfig.js
@@ -1,6 +1,6 @@
 module.exports = Object.freeze({
     DB_HOST : 'database-1.c380a08uukyc.ap-south-1.rds.amazonaws.com',
     DB_USER : 'admin',
-    DB_PWD : 'Admin1234',
+    DB_PWD : '<YOUR DB PASSWORD>',
     DB_DATABASE : 'webappdb'
 });
